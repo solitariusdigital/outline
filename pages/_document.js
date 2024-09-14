d399ff546f10513f9d54343051d19cca0ba3ddc2
Pouyan Madanipour <pouyan.mad@gmail.com>
2024-09-14 22:16:06 +0330

    update:

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#fff" />
+          <meta name="theme-color" content="#2d2b7f" />
         </Head>
         <body>
           <Main />
