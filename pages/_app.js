098cd160d1b341899f1d6f3046842f7d90ac44d1
Pouyan Madanipour <pouyan.mad@gmail.com>
2026-07-25 09:21:20 +0330

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -12,11 +12,11 @@ export default function App({ Component, pageProps }) {
             name="viewport"
             content="width=device-width, initial-scale=1, viewport-fit=cover"
           />
-          <meta name="theme-color" content="#000b1f" />
+          <meta name="theme-color" content="#fafbf6" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
-            content="black-translucent"
+            content="default"
           />
         </Head>
         <Component {...pageProps} />
